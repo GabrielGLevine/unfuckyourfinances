5b56486f4183282dd4d413ad8e31d10b6131d8e9
GabrielGLevine <gabrielglevine@gmail.com>
2018-09-22 19:33:29 -0500

    add nav bar

---
+++ app/javascript/controllers/main_controller.js
@@ -0,0 +1,33 @@
+import fontawesome from '@fortawesome/fontawesome'
+import icons from '@fortawesome/fontawesome-free-regular'
+import { Controller } from 'stimulus'
+export default class extends Controller {
+  initialize() {
+    fontawesome.library.add(icons)
+  }
+  connect() {
+    fontawesome.dom.i2svg()
+
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+      // Add a click event on each of them
+      $navbarBurgers.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+
+          // Get the target from the "data-target" attribute
+          var target = $el.dataset.target;
+          var $target = document.getElementById(target);
+
+          // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+          $el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+
+        });
+      });
+    }
+  }
+}
